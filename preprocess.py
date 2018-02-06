309df83ffc5ed258648a7c2b63454477d3536f44
Hemant Mangla <hemantmangla78@gmail.com>
2018-02-06 17:30:08 +0530

    feat: add preprocessing function
    comvert the train and dev data from json file into four separate files

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 import os
 from urllib import urlretrieve
+import json
 from tqdm import tqdm
 squad_url = 'https://rajpurkar.github.io/SQuAD-explorer/dataset/'
 
@@ -39,6 +40,68 @@ def download_from_url(download_dir, download_url, file_name, file_size=None):
         os.remove(full_file_name)
         raise Exception("Unexpected datasize of the file! Please get the file using browser of try again..")
 
+    return full_file_name
+
+def read_json(file_dir, file_name):
+    with open(os.path.join(file_dir, file_name), 'r') as json_file:
+        data_loaded = json.load(json_file)
+    return data_loaded
+
+def convert_data(data, directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    contexts_filename = os.path.join(directory, 'contexts')
+    questions_filename = os.path.join(directory, 'questions')
+    answers_filename = os.path.join(directory, 'answers')
+    span_filename = os.path.join(directory, 'spans')
+    
+    qn = 0
+    an = 0
+    skipped = 0
+    typ = directory.split('/')[-1]
+    with open(contexts_filename, 'w+') as context_file, open(questions_filename, 'w+') as question_file, open(answers_filename,'w+') as answer_file, open(span_filename, 'w+') as span_file:
+
+        for articleId in tqdm(range(len(data['data'])), desc="Preprocessing {}".format(typ)):
+            articles  = data['data'][articleId]['paragraphs']
+            
+            for contextId in range(len(articles)):
+                context = articles[contextId]['context']
+                qas = articles[contextId]['qas']
+                
+                for questionId in range(len(qas)):
+                    question = qas[questionId]['question']
+                    answers = qas[questionId]['answers']
+                    
+                    qn += 1
+                    for answerId in range(1):
+                        answer_txt = answers[answerId]['text']
+
+                        answer_start = answers[answerId]['answer_start']
+                        answer_end = answer_start + len(answer_txt)
+                        
+                        try:
+                            check = context[answer_end]
+                            context_file.write(context + '\n')
+                            question_file.write(question + '\n')
+                            answer_file.write(answer_txt + '\n')
+                            span_file.write("( " + str(answer_start) + " " + str(answer_end) +  ")\n") 
+
+                        except Exception as e:
+                            skipped += 1
+                        
+                        an += 1
+    print "Total questions skipped in {} is {}".format(typ, skipped)
+    return qn,an
+
+def preprocess(download_dir, process_dir, filename, filesize, tier):
+
+    download_from_url(download_dir, squad_url, filename, filesize)
+    data = read_json(download_dir, filename)
+
+    num_ques, num_ans = convert_data(data, os.path.join(process_dir, tier))
+
+    return num_ques, num_ans
+
 if __name__=='__main__':
 
     downloaded_data_dir = os.path.join('data', 'download', 'squad')
@@ -47,4 +110,12 @@ if __name__=='__main__':
     print "Downloading the file into {}".format(downloaded_data_dir)
     
     dev_file = 'dev-v1.1.json'
-    download_from_url(downloaded_data_dir, squad_url, dev_file, 4854279L)
+    train_file = 'train-v1.1.json'
+
+    #Downloading and preprocessing dev data
+    num_ques_dev, _ = preprocess(downloaded_data_dir, processed_data_dir, dev_file, 4854279L, 'dev')
+    print "Total dev questions {}".format(num_ques_dev)
+
+    #Downloading and preprocessing train data
+    num_ques_train, _ = preprocess(downloaded_data_dir, processed_data_dir, train_file, 30288272L, 'train')
+    print "Total train questions {}".format(num_ques_train)
