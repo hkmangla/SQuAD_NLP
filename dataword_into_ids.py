826f252de649ce1894120077841794ea64a059bf
Hemant Mangla <hemantmangla78@gmail.com>
2018-02-10 22:31:14 +0530

    bug: add basic tokenizer to tokenize the sentences

--- dataword_into_ids.py
+++ dataword_into_ids.py
@@ -1,6 +1,25 @@
 import os
 import numpy as np
+import re
 
+def tokenizer(sentence):
+    tokens = []
+    for word in sentence.strip().split():
+        tokens.extend(re.split(" ", word))
+
+    return [w for w in tokens if w]
+
+def initialize_vocab(vocab_path):
+    if os.path.exists(vocab_path):
+        rev_vocab = []
+        with open(vocab_path, "r") as f:
+            for line in f:
+                rev_vocab.append(line.strip('\n'))
+        vocab = dict([(x,y) for (y,x) in enumerate(rev_vocab)])
+        return vocab, rev_vocab
+
+    else:
+        return ValueError("File {} does not found".format(vocab_path))
 
 def create_vocabulary(vocab_dir, data_paths):
     if not os.path.exists(os.path.join(vocab_dir, 'vocab.dat')):
@@ -11,7 +30,7 @@ def create_vocabulary(vocab_dir, data_paths):
             with open(path, 'r') as f:
                 counter = 0
                 for line in f:
-                    words = line.split(' ')
+                    words = tokenizer(line)
                     for word in words:
                         if word in vocab:
                             vocab[word] += 1
@@ -21,7 +40,7 @@ def create_vocabulary(vocab_dir, data_paths):
         vocab_list = sorted(vocab, key=vocab.get, reverse=True)
         with open(os.path.join(vocab_dir, 'vocab.dat'), 'w') as fh:
             for w in vocab_list:
-                fh.write(w + "\n")
+                fh.write(w + b"\n")
                     
 if __name__=='__main__':
 
@@ -34,4 +53,6 @@ if __name__=='__main__':
                                   train_dir + '/answers',
                                   dev_dir + '/contexts',
                                   dev_dir + '/answers'])
+
+    vocab, _ = initialize_vocab(vocab_dir + '/vocab.dat')
 
