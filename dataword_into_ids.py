bc93543839fffaf8236f3a18db7343412842111d
Hemant Mangla <hemantmangla78@gmail.com>
2018-02-11 18:34:07 +0530

    feat: add function to trimmed glove file according to data

--- dataword_into_ids.py
+++ dataword_into_ids.py
@@ -1,6 +1,21 @@
 import os
 import numpy as np
 import re
+from tqdm import tqdm
+
+def binary_search(lst, target):
+    low = 0
+    high = len(lst) - 1
+    while low < high:
+        avg = (low + high) / 2
+        if lst[avg][0] == target:
+            return lst[avg][1]
+        elif lst[avg][0] < target:
+            low = avg + 1
+        else:
+            high = avg - 1
+
+    return False
 
 def tokenizer(sentence):
     tokens = []
@@ -41,7 +56,53 @@ def create_vocabulary(vocab_dir, data_paths):
         with open(os.path.join(vocab_dir, 'vocab.dat'), 'w') as fh:
             for w in vocab_list:
                 fh.write(w + b"\n")
+    else:
+        print "Vocabulary is already created!"
                     
+def process_glove(glove_dir, vocab, glove_dim):
+    vocab_with_idx = [(value, counter) for counter, value in enumerate(vocab)]
+    vocab_with_idx.sort()
+
+    if not os.path.exists(os.path.join(glove_dir, "glove.trimmered_{}.npz".format(glove_dim))):
+        glove_path = os.path.join(glove_dir, 'glove.6B.{}d.txt'.format(glove_dim))
+        
+        glove = np.zeros((len(vocab), glove_dim))
+        found = 0
+
+        with open(glove_path, 'r') as f:
+            for line in tqdm(f, total=4e5):
+                arr  = line.strip().split()
+                word = arr[0]
+                vector = list(map(float, arr[1:]))
+                
+                find = False
+                idx = binary_search(vocab_with_idx, word)
+                if idx:
+                    find = True
+                    glove[idx, :] = vector
+                    found += 1
+                
+                idx = binary_search(vocab_with_idx, word.capitalize())
+                if idx:
+                    find = True
+                    glove[idx, :] = vector 
+                    found += 1
+                
+                idx = binary_search(vocab_with_idx, word.upper())
+                if idx: 
+                    find = True
+                    glove[idx, :] = vector 
+                    found += 1
+
+                if find and found < 10000:
+                    print word
+
+        print "{} word out of {} word found in glove data".format(found, len(vocab))
+        np.savez_compressed(os.path.join(glove_dir, "glove.trimmered_{}".format(glove_dim)))
+    
+    else:
+        print "file already presents"
+    
 if __name__=='__main__':
 
     train_dir = os.path.join('data/processed/squad', 'train')
@@ -54,5 +115,8 @@ if __name__=='__main__':
                                   dev_dir + '/contexts',
                                   dev_dir + '/answers'])
 
-    vocab, _ = initialize_vocab(vocab_dir + '/vocab.dat')
+    vocab, rev_vocab = initialize_vocab(vocab_dir + '/vocab.dat')
+
+    print len(rev_vocab)
+    process_glove(glove_dir, rev_vocab, 100)
 
