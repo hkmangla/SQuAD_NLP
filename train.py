df6cdcd224254c1be0805cd7335b4f661264ebed
Hemant Mangla <hemantmangla78@gmail.com>
2018-04-08 16:00:22 +0530

    feat: add logger function and add file to split train and val data

--- train.py
+++ train.py
@@ -7,7 +7,37 @@ from encoder import Encoder
 from decoder import Decoder
 from qa_system import QASystem
 
+import logging
 
+def set_logger(file_path):
+    logger = logging.getLogger('tensorflow')
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(os.path.join(file_path,'log.txt'))
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    return logger
+
+logger = set_logger("log.txt")
+
+def initialize_model(session, model, train_dir){
+    ckpt = tf.train.get_checkpoint_state(train_dir)
+        
+    if ckpt and os.path.exists(ckpt.model_checkpoint_path):
+        logger.info("Reading model parameters from {}".format(ckpt.model_checkpoint_path))
+        model.saver.restore(session, ckpt.model_checkpoint_path)
+
+    else:
+        logger.info("Initializing model with fresh parameters")
+        session.run(tf.global_variables_intializer())
+        
+    return model
+}
 def main(_):
     data_dir = os.path.join('data','processed','squad') 
     train_data, val_data = load_data(data_dir, max_context_len, max_question_len)
@@ -26,8 +56,12 @@ def main(_):
     decoder = Decoder(args)
 
     qa = QASystem(encoder, decoder, embeddings, vocab)
-
+    
+    train_dir = os.path.join("train")
     #Here training part
-    #TODO
-
+    
+    with tf.Session() as sess:
+        initialize_model(sess, qa, train_dir)
+        
+        qa.train(sess, train_data, val_data, train_dir)
 
