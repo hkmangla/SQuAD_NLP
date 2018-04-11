fa7093fb41575e60556ecb9ef864718e9927925b
Hemant Mangla <hemantmangla78@gmail.com>
2018-04-11 15:32:51 +0530

    feat: add logging handler function

--- tools/utils.py
+++ tools/utils.py
@@ -1,4 +1,32 @@
+import logging
 import os
+
+def set_logger(file_path):
+    logger = logging.getLogger('tensorflow')
+    logger.setLevel(logging.INFO)
+    
+    file_handler = logging.FileHandler(file_path)
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    return logger
+
+def initialize_vocab(vocab_path):
+    if os.path.exists(vocab_path):
+        rev_vocab = []
+        with open(vocab_path, "r") as f:
+            for line in f:
+                rev_vocab.append(line.strip('\n'))
+        vocab = dict([(x,y) for (y,x) in enumerate(rev_vocab)])
+        return vocab, rev_vocab
+
+    else:
+        return ValueError("File {} does not found".format(vocab_path))
+
 
 def read(data_file, size):
     count = 0
@@ -24,7 +52,7 @@ def pad_sequence(data, max_len):
             mask = [True]*max_len
         else:
             pad_vect = sentence + [0]*(max_len - len(sentence))
-            mask = [True]*len(sentence) + [False]*(max_len - len(sentence)
+            mask = [True]*len(sentence) + [False]*(max_len - len(sentence))
 
     return pad_vect, mask
 
@@ -47,16 +75,29 @@ def process_span(span_data, max_context_len):
 
 def load_data(data_dir, max_context_len, max_question_len, size=None):
 
-    train_context = read(os.path.join(data_dir, 'train/ids.contexts'), size)
-    train_question = read(os.path.join(data_dir, 'train/ids.questions'), size)
-    train_span = read(os.path.join(data_dir, 'train/spans'), size)
+    train_context = read(os.path.join(data_dir, 'train.ids.contexts'), size)
+    train_question = read(os.path.join(data_dir, 'train.ids.questions'), size)
+    train_span = read(os.path.join(data_dir, 'train.spans'), size)
 
     train_context_data, train_context_mask = pad_sequence(train_context,max_context_len)
     train_qustion_data, train_question_mask = pad_sequence(train_question, max_question_len)
-    star_span_vector, end_span_vector = process_span(train_span, max_context_len)
+    start_span_vector_train, end_span_vector_train = process_span(train_span, max_context_len)
     
     train_data = list(zip([train_context_data, train_context_mask, train_question_data,
-                          train_question_mask, start_span_vector,end_span_vector, span])
+                          train_question_mask, start_span_vector_train ,end_span_vector, train_span]))
+
+    val_context = read(os.path.join(data_dir, 'val.ids.contexts'), size)
+    val_question = read(os.path.join(data_dir, 'val.ids.questions'), size)
+    val_span = read(os.path.join(data_dir, 'val.spans'), size)
+
+    val_context_data, val_context_mask = pad_sequence(val_context,max_context_len)
+    val_qustion_data, val_question_mask = pad_sequence(val_question, max_question_len)
+    start_span_vector_val, end_span_vector_val = process_span(val_span, max_context_len)
+    
+    val_data = list(zip([val_context_data, val_context_mask, val_question_data,
+                          val_question_mask, start_span_vector_val, end_span_vector_val, val_span]))
+    
+    return train_data, val_data
 
 def load_embeddings(emd_dir):
     return np.load(dir)['glove']
--- train.py
+++ train.py
@@ -2,30 +2,15 @@ import tensorflow as tf
 import sys
 sys.path.append('tools/')
 from utils import *
-from dataword_into_ids import initialize_vocab
 from encoder import Encoder
 from decoder import Decoder
 from qa_system import QASystem
 
 import logging
 
-def set_logger(file_path):
-    logger = logging.getLogger('tensorflow')
-    logger.setLevel(logging.INFO)
+logger = set_logger("train/log.txt")
 
-    file_handler = logging.FileHandler(os.path.join(file_path,'log.txt'))
-    file_handler.setLevel(logging.INFO)
-
-    formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-
-    return logger
-
-logger = set_logger("log.txt")
-
-def initialize_model(session, model, train_dir){
+def initialize_model(session, model, train_dir):
     ckpt = tf.train.get_checkpoint_state(train_dir)
         
     if ckpt and os.path.exists(ckpt.model_checkpoint_path):
@@ -37,9 +22,10 @@ def initialize_model(session, model, train_dir){
         session.run(tf.global_variables_intializer())
         
     return model
-}
+
 def main(_):
-    data_dir = os.path.join('data','processed','squad') 
+
+    data_dir = os.path.join('data','processed','squad','train') 
     train_data, val_data = load_data(data_dir, max_context_len, max_question_len)
     
     vocab_path = os.path.join('data','processed','vocab','vocab.dat')
