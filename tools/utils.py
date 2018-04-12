63984d0a1790305efaa8f278de10f0749abf1a86
Hemant Mangla <hemantmangla78@gmail.com>
2018-04-12 15:58:37 +0530

    feat: all other files and function added

--- tools/utils.py
+++ tools/utils.py
@@ -1,5 +1,8 @@
 import logging
 import os
+import numpy as np
+import time
+import tensorflow as tf
 
 def set_logger(file_path):
     logger = logging.getLogger('tensorflow')
@@ -34,7 +37,7 @@ def read(data_file, size):
 
     with open(data_file,'r') as f:
         for line in f:
-            data.append(list(map(lambda x: int(x), line.strip.split())))
+            data.append(list(map(lambda x: int(x), line.strip().split())))
             count += 1
 
             if size is not None:
@@ -45,16 +48,26 @@ def read(data_file, size):
 
 def pad_sequence(data, max_len):
     mask = []
-    pad_vec = []
+    pad_vect = []
     for sentence in data:
-        if len(vect) >= max_len:
-            pad_vect = sentence[:max_len]
-            mask = [True]*max_len
+        if len(sentence) >= max_len:
+            pad_vect.append(sentence[:max_len])
+            mask.append([True]*max_len)
         else:
-            pad_vect = sentence + [0]*(max_len - len(sentence))
-            mask = [True]*len(sentence) + [False]*(max_len - len(sentence))
+            pad_vect.append(sentence + [0]*(max_len - len(sentence)))
+            mask.append([True]*len(sentence) + [False]*(max_len - len(sentence)))
 
     return pad_vect, mask
+
+def variable_summaries(var, name_scope, matrix = True):
+  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+
+  with tf.name_scope(name_scope):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    if matrix:
+        norm = tf.sqrt(tf.reduce_sum(var * var))
+        tf.summary.scalar('norm', norm)
 
 def process_span(span_data, max_context_len):
     start_span_vector = []
@@ -73,32 +86,200 @@ def process_span(span_data, max_context_len):
 
     return start_span_vector, end_span_vector
 
-def load_data(data_dir, max_context_len, max_question_len, size=None):
+def load_data(data_dir, max_context_len, max_question_len, size=100):
 
     train_context = read(os.path.join(data_dir, 'train.ids.contexts'), size)
     train_question = read(os.path.join(data_dir, 'train.ids.questions'), size)
     train_span = read(os.path.join(data_dir, 'train.spans'), size)
 
     train_context_data, train_context_mask = pad_sequence(train_context,max_context_len)
-    train_qustion_data, train_question_mask = pad_sequence(train_question, max_question_len)
+    train_question_data, train_question_mask = pad_sequence(train_question, max_question_len)
     start_span_vector_train, end_span_vector_train = process_span(train_span, max_context_len)
     
-    train_data = list(zip([train_context_data, train_context_mask, train_question_data,
-                          train_question_mask, start_span_vector_train ,end_span_vector, train_span]))
+    train_data = vectorize(train_context_data, train_context_mask, train_question_data,
+                          train_question_mask, start_span_vector_train ,end_span_vector_train, train_span)
 
     val_context = read(os.path.join(data_dir, 'val.ids.contexts'), size)
     val_question = read(os.path.join(data_dir, 'val.ids.questions'), size)
     val_span = read(os.path.join(data_dir, 'val.spans'), size)
 
     val_context_data, val_context_mask = pad_sequence(val_context,max_context_len)
-    val_qustion_data, val_question_mask = pad_sequence(val_question, max_question_len)
+    val_question_data, val_question_mask = pad_sequence(val_question, max_question_len)
     start_span_vector_val, end_span_vector_val = process_span(val_span, max_context_len)
     
-    val_data = list(zip([val_context_data, val_context_mask, val_question_data,
-                          val_question_mask, start_span_vector_val, end_span_vector_val, val_span]))
+    val_data = vectorize(val_context_data, val_context_mask, val_question_data,
+                          val_question_mask, start_span_vector_val, end_span_vector_val, val_span)
     
     return train_data, val_data
 
-def load_embeddings(emd_dir):
-    return np.load(dir)['glove']
+def vectorize(*args):
+    return list(zip(*args))
 
+def load_embeddings(emd_dir):
+    return np.load(emd_dir)['glove']
+
+class Progbar(object):
+    """
+    Progbar class copied from keras (https://github.com/fchollet/keras/)
+    Displays a progress bar.
+    # Arguments
+        target: Total number of steps expected.
+        interval: Minimum visual progress update interval (in seconds).
+    """
+
+    def __init__(self, target, width=30, verbose=1):
+        self.width = width
+        self.target = target
+        self.sum_values = {}
+        self.unique_values = []
+        self.start = time.time()
+        self.total_width = 0
+        self.seen_so_far = 0
+        self.verbose = verbose
+
+    def update(self, current, values=None, exact=None):
+        """
+        Updates the progress bar.
+        # Arguments
+            current: Index of current step.
+            values: List of tuples (name, value_for_last_step).
+                The progress bar will display averages for these values.
+            exact: List of tuples (name, value_for_last_step).
+                The progress bar will display these values directly.
+        """
+        values = values or []
+        exact = exact or []
+
+        for k, v in values:
+            if k not in self.sum_values:
+                self.sum_values[k] = [v * (current - self.seen_so_far), current - self.seen_so_far]
+                self.unique_values.append(k)
+            else:
+                self.sum_values[k][0] += v * (current - self.seen_so_far)
+                self.sum_values[k][1] += (current - self.seen_so_far)
+        for k, v in exact:
+            if k not in self.sum_values:
+                self.unique_values.append(k)
+            self.sum_values[k] = [v, 1]
+        self.seen_so_far = current
+
+        now = time.time()
+        if self.verbose == 1:
+            prev_total_width = self.total_width
+            sys.stdout.write("\b" * prev_total_width)
+            sys.stdout.write("\r")
+
+            numdigits = int(np.floor(np.log10(self.target))) + 1
+            barstr = '%%%dd/%%%dd [' % (numdigits, numdigits)
+            bar = barstr % (current, self.target)
+            prog = float(current)/self.target
+            prog_width = int(self.width*prog)
+            if prog_width > 0:
+                bar += ('='*(prog_width-1))
+                if current < self.target:
+                    bar += '>'
+                else:
+                    bar += '='
+            bar += ('.'*(self.width-prog_width))
+            bar += ']'
+            sys.stdout.write(bar)
+            self.total_width = len(bar)
+
+            if current:
+                time_per_unit = (now - self.start) / current
+            else:
+                time_per_unit = 0
+            eta = time_per_unit*(self.target - current)
+            info = ''
+            if current < self.target:
+                info += ' - ETA: %ds' % eta
+            else:
+                info += ' - %ds' % (now - self.start)
+            for k in self.unique_values:
+                if isinstance(self.sum_values[k], list):
+                    info += ' - %s: %.4f' % (k, self.sum_values[k][0] / max(1, self.sum_values[k][1]))
+                else:
+                    info += ' - %s: %s' % (k, self.sum_values[k])
+
+            self.total_width += len(info)
+            if prev_total_width > self.total_width:
+                info += ((prev_total_width-self.total_width) * " ")
+
+            sys.stdout.write(info)
+            sys.stdout.flush()
+
+            if current >= self.target:
+                sys.stdout.write("\n")
+
+        if self.verbose == 2:
+            if current >= self.target:
+                info = '%ds' % (now - self.start)
+                for k in self.unique_values:
+                    info += ' - %s: %.4f' % (k, self.sum_values[k][0] / max(1, self.sum_values[k][1]))
+                sys.stdout.write(info + "\n")
+
+    def add(self, n, values=None):
+        self.update(self.seen_so_far+n, values)
+
+
+def get_minibatches(data, minibatch_size, shuffle=True):
+    """
+    Iterates through the provided data one minibatch at at time. You can use this function to
+    iterate through data in minibatches as follows:
+        for inputs_minibatch in get_minibatches(inputs, minibatch_size):
+            ...
+    Or with multiple data sources:
+        for inputs_minibatch, labels_minibatch in get_minibatches([inputs, labels], minibatch_size):
+            ...
+    Args:
+        data: there are two possible values:
+            - a list or numpy array
+            - a list where each element is either a list or numpy array
+        minibatch_size: the maximum number of items in a minibatch
+        shuffle: whether to randomize the order of returned data
+    Returns:
+        minibatches: the return value depends on data:
+            - If data is a list/array it yields the next minibatch of data.
+            - If data a list of lists/arrays it returns the next minibatch of each element in the
+              list. This can be used to iterate through multiple data sources
+              (e.g., features and labels) at the same time.
+    """
+    list_data = type(data) is list and (type(data[0]) is list or type(data[0]) is np.ndarray)
+    data_size = len(data[0]) if list_data else len(data)
+    indices = np.arange(data_size)
+    if shuffle:
+        np.random.shuffle(indices)
+    for minibatch_start in np.arange(0, data_size, minibatch_size):
+        minibatch_indices = indices[minibatch_start:minibatch_start + minibatch_size]
+        yield [minibatch(d, minibatch_indices) for d in data] if list_data \
+            else minibatch(data, minibatch_indices)
+
+
+def minibatch(data, minibatch_idx):
+    return data[minibatch_idx] if type(data) is np.ndarray else [data[i] for i in minibatch_idx]
+
+def minibatches(data, batch_size, shuffle=True):
+    batches = [np.array(col) for col in zip(*data)]
+    return get_minibatches(batches, batch_size, shuffle)
+
+def print_sentence(output, sentence, labels, predictions):
+
+    spacings = [max(len(sentence[i]), len(labels[i]), len(predictions[i])) for i in range(len(sentence))]
+    # Compute the word spacing
+    output.write("x : ")
+    for token, spacing in zip(sentence, spacings):
+        output.write(token)
+        output.write(" " * (spacing - len(token) + 1))
+    output.write("\n")
+
+    output.write("y*: ")
+    for token, spacing in zip(labels, spacings):
+        output.write(token)
+        output.write(" " * (spacing - len(token) + 1))
+    output.write("\n")
+
+    output.write("y': ")
+    for token, spacing in zip(predictions, spacings):
+        output.write(token)
+        output.write(" " * (spacing - len(token) + 1))
+        output.write("\n")
--- train.py
+++ train.py
@@ -10,6 +10,32 @@ import logging
 
 logger = set_logger("train/log.txt")
 
+model_name = '/baseline4'
+tf.app.flags.DEFINE_float("base_lr", 0.001, "Basic learning rate.")
+tf.app.flags.DEFINE_float("decay_rate", 0.9, "Decay ratio after a certain batch number.")
+tf.app.flags.DEFINE_integer("decay_number", 600, "Anneal learning rate after this number of batches.")
+tf.app.flags.DEFINE_float("max_grad_norm", 10.0, "Clip gradients to this norm.")
+tf.app.flags.DEFINE_float("dropout", 0.4, "Fraction of units randomly dropped on non-recurrent connections.")
+tf.app.flags.DEFINE_integer("batch_size", 128, "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("epochs", 15, "Number of epochs to train.")
+tf.app.flags.DEFINE_integer("state_size", 100, "Size of each model layer.")
+tf.app.flags.DEFINE_integer("output_size", 2, "The output size of your model.")
+tf.app.flags.DEFINE_integer("embedding_size", 100, "Size of the pretrained vocabulary.")
+tf.app.flags.DEFINE_integer("max_context_len", 600, "max length of the context input")
+tf.app.flags.DEFINE_integer("max_question_len", 100, "max length of the question input")
+tf.app.flags.DEFINE_string("data_dir", "data/squad", "SQuAD directory (default ./data/squad)")
+tf.app.flags.DEFINE_string("train_dir", "train", "Training directory to save the model parameters (default: ./train).")
+tf.app.flags.DEFINE_string("load_train_dir", "", "Training directory to load model parameters from to resume training (default: {train_dir}).")
+tf.app.flags.DEFINE_string("log_dir", "log" + model_name, "Path to store log and flag files (default: ./log)")
+tf.app.flags.DEFINE_string("vocab_path", "data/squad/vocab.dat", "Path to vocab file (default: ./data/squad/vocab.dat)")
+tf.app.flags.DEFINE_string("embed_path", "", "Path to the trimmed GLoVe embedding (default: ./data/squad/glove.trimmed.{embedding_size}.npz)")
+tf.app.flags.DEFINE_string("model_name", model_name, "name of the model")
+tf.app.flags.DEFINE_string('summary_dir', 'summary' + model_name, 'tensorboard summary dir')
+tf.app.flags.DEFINE_bool('summary_flag', True, 'if true log summary')
+
+
+FLAGS = tf.app.flags.FLAGS
+
 def initialize_model(session, model, train_dir):
     ckpt = tf.train.get_checkpoint_state(train_dir)
         
@@ -19,29 +45,30 @@ def initialize_model(session, model, train_dir):
 
     else:
         logger.info("Initializing model with fresh parameters")
-        session.run(tf.global_variables_intializer())
+        session.run(tf.initialize_all_variables())
         
     return model
 
 def main(_):
 
     data_dir = os.path.join('data','processed','squad','train') 
-    train_data, val_data = load_data(data_dir, max_context_len, max_question_len)
+    train_data, val_data = load_data(data_dir, FLAGS.max_context_len, FLAGS.max_question_len)
     
     vocab_path = os.path.join('data','processed','vocab','vocab.dat')
     vocab, rev_vocab = initialize_vocab(vocab_path)
 
     embd_dir = os.path.join('data','download','glove','glove.trimmered_100.npz')
+    print embd_dir
     embed = load_embeddings(embd_dir)
 
-    emdeddings = tf.constant(embed, tf.float32)
+    embeddings = tf.constant(embed, tf.float32)
     #Dummy code... must have to edit
 
     #Below functions are not implemented till
-    encoder = Encoder(args)
-    decoder = Decoder(args)
+    encoder = Encoder(FLAGS.state_size, FLAGS.summary_flag, FLAGS.max_context_len, FLAGS.max_question_len)
+    decoder = Decoder(FLAGS.state_size, FLAGS.summary_flag)
 
-    qa = QASystem(encoder, decoder, embeddings, vocab)
+    qa = QASystem(encoder, decoder, FLAGS, embeddings, vocab)
     
     train_dir = os.path.join("train")
     #Here training part
@@ -51,3 +78,5 @@ def main(_):
         
         qa.train(sess, train_data, val_data, train_dir)
 
+if __name__ == '__main__':
+    tf.app.run()
